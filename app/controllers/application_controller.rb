f03fa8ecf5a897c70df69fde68504bad12144e70
EC2 Default User <ec2-user@ip-172-31-4-31.ap-northeast-1.compute.internal>
2022-10-19 07:43:16 +0000

    [update7 ユーザログイン成功したとき]Bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    flash[:notice] = "Signed in successfully."
+    user_path(current_user.id)
   end
 
   def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
     root_path
   end
+
 
   protected
 
